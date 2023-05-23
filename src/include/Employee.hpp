cfc20a5113c825ee7c4e05ae49b165a0393bc91c
GuuscoNL <99217547+GuuscoNL@users.noreply.github.com>
2023-05-23 09:43:19 +0200

    Employee Doxygen

--- src/include/Employee.hpp
+++ src/include/Employee.hpp
@@ -10,16 +10,43 @@ private:
 	bool forkliftCertificate;
 
 public:
+
+	/**
+	* @brief Get name of Employee
+	* @return Name of Employee
+	*/
 	const std::string getName();
 
+	/**
+	* @brief Get busy status of Employee
+	* @return busy status of Employee
+	*/
 	bool getBusy();
 
+	/**
+	* @brief Set busy status of Employee
+	* @param busy The busy status of the Employee
+	*/
 	void setBusy(bool busy);
 
+	/**
+	* @brief Set ForkliftCertificate of Employee
+	* @param forkliftCertificate The certification of the Employee
+	*/
 	bool getForkliftCertificate();
 
+	/**
+	* @brief Set ForkliftCertificate of Employee
+	* @param forkliftCertificate The certification of the Employee
+	*/
 	void setForkliftCertificate(bool forkliftCertificate);
 
+	/**
+	* @brief Employee class
+	* @param name Name of Employee
+	* @param forkliftCertificate Is the Employee certified?
+	* @return The Employee
+	*/
 	Employee(std::string name, bool forkliftCertificate);
 };
 
