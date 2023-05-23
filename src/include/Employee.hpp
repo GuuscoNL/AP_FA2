d8318733c68ce2fb5f2a9fcd0f9507423cfc7042
GuuscoNL <99217547+GuuscoNL@users.noreply.github.com>
2023-05-23 09:49:25 +0200

    Pallet dec gedeeltelijk

--- src/include/Employee.hpp
+++ src/include/Employee.hpp
@@ -12,40 +12,41 @@ private:
 public:
 
 	/**
-	* @brief Get name of Employee
-	* @return Name of Employee
-	*/
+	 * @brief Get name of Employee
+	 * 
+	 * @return Name of Employee
+	 */
 	const std::string getName();
 
 	/**
-	* @brief Get busy status of Employee
-	* @return busy status of Employee
-	*/
+	 * @brief Get busy status of Employee
+	 * 
+	 * @return busy status of Employee
+	 */
 	bool getBusy();
 
 	/**
-	* @brief Set busy status of Employee
-	* @param busy The busy status of the Employee
-	*/
+	 * @brief Set busy status of Employee
+	 * @param busy The busy status of the Employee
+	 */
 	void setBusy(bool busy);
 
 	/**
-	* @brief Set ForkliftCertificate of Employee
-	* @param forkliftCertificate The certification of the Employee
-	*/
+	 * @brief Set ForkliftCertificate of Employee
+	 * @param forkliftCertificate The certification of the Employee
+	 */
 	bool getForkliftCertificate();
 
 	/**
-	* @brief Set ForkliftCertificate of Employee
-	* @param forkliftCertificate The certification of the Employee
-	*/
+	 * @brief Set ForkliftCertificate of Employee
+	 * @param forkliftCertificate The certification of the Employee
+	 */
 	void setForkliftCertificate(bool forkliftCertificate);
 
 	/**
-	* @brief Employee class
-	* @param name Name of Employee
-	* @param forkliftCertificate Is the Employee certified?
-	* @return The Employee
+	 * @brief Employee class
+	 * @param name Name of Employee
+	 * @param forkliftCertificate Is the Employee certified?
 	*/
 	Employee(std::string name, bool forkliftCertificate);
 };
--- src/include/Pallet.hpp
+++ src/include/Pallet.hpp
@@ -11,14 +11,39 @@ private:
 	int itemCount;
 
 public:
+	/**
+	 * @brief Get ItemCount of the Pallet
+	 * 
+	 * @return ItemCount of the Pallet
+	 */
 	int getItemCount();
 
+	/**
+	 * @brief Get ItemName of the Pallet
+	 * 
+	 * @return ItemName of the Pallet
+	 */
 	std::string getItemName();
 
+	/**
+	 * @brief Get ItemCapacity of the Pallet
+	 * 
+	 * @return ItemCapacity of the Pallet 
+	 */
 	int getItemCapacity();
 
+	/**
+	 * @brief Pallet class
+	 * 
+	 */
 	Pallet();
 
+	/**
+	 * @brief Pallet class
+	 * @param itemName The name od the item the Pallet is storing.
+	 * @param itemCapacity The max capacity the Pallet can hold of 1 specific item.
+	 * @param itemCount The amount of items the Pallet has stored.
+	 */
 	Pallet(std::string itemName, int itemCapacity, int itemCount);
 
 	bool isEmpty() override;
