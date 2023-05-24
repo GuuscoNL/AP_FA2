5a0afeb7bbe40cb1b857044f80710a5174d3bb79
GuuscoNL <99217547+GuuscoNL@users.noreply.github.com>
2023-05-24 21:43:33 +0200

    Pallet comments

--- src/include/Pallet.hpp
+++ src/include/Pallet.hpp
@@ -12,42 +12,78 @@ private:
 
 public:
 	/**
-	 * @brief Get ItemCount of the Pallet
+	 * @brief Get ItemCount of the Pallet.
 	 * 
-	 * @return ItemCount of the Pallet
+	 * @return ItemCount of the Pallet.
 	 */
 	int getItemCount();
 
 	/**
-	 * @brief Get ItemName of the Pallet
+	 * @brief Get ItemName of the Pallet.
 	 * 
-	 * @return ItemName of the Pallet
+	 * @return ItemName of the Pallet.
 	 */
 	std::string getItemName();
 
 	/**
-	 * @brief Get ItemCapacity of the Pallet
+	 * @brief Get ItemCapacity of the Pallet.
 	 * 
-	 * @return ItemCapacity of the Pallet 
+	 * @return ItemCapacity of the Pallet .
 	 */
 	int getItemCapacity();
 
 	/**
-	 * @brief Pallet class
+	 * @brief Construct a new Pallet.
 	 */
 	Pallet();
 
 	/**
-	 * @brief Pallet class
-	 * @param itemName The name od the item the Pallet is storing.
+	 * @brief Construct a new Pallet.
+	 * @param itemName The name of the item the Pallet is storing.
 	 * @param itemCapacity The max capacity the Pallet can hold of 1 specific item.
 	 * @param itemCount The amount of items the Pallet has stored.
 	 */
 	Pallet(std::string itemName, int itemCapacity, int itemCount);
 
+	/**
+	 * @brief Is the Pallet empty?
+	 * 
+	 * @return `true`: The Pallet is empty.
+	 * @return `false`: The Pallet is not empty,
+	 */
 	bool isEmpty() override;
+
+	/**
+	 * @brief Is the Pallet full?
+	 * 
+	 * @return `true`: The Pallet is full.
+	 * @return `false`: The Pallet is not full.
+	 */
 	bool isFull() override;
+
+	/**
+	 * @brief Reallocate Pallet for a different item.
+	 * 
+	 * @param itemName The new item to store.
+	 * @param itemCapacity The new max capacity of the Pallet.
+	 * @return true Pallet has been successfully reallocated.
+	 * @return false Pallet has not been successfully reallocated, because the Pallet is not empty.
+	 */
 	bool reallocateEmptyPallet(std::string itemName, int itemCapacity);
+
+	/**
+	 * @brief Take one item from the Pallet.
+	 * 
+	 * @return `true`: Item was taken successfully.
+	 * @return `false`: Item was not taken successfully, because the Pallet is empty.
+	 */
 	bool takeOne();
+
+	/**
+	 * @brief Put one item on the Pallet.
+	 * 
+	 * @return `true`: Item was added successfully.
+	 * @return `false`: Item was not added successfully, because the Pallet is full.
+	 */
 	bool putOne();
 };
