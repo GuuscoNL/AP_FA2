15bba7302c186c6fd7793be488c0b5eca5f3e0e2
GuuscoNL <99217547+GuuscoNL@users.noreply.github.com>
2023-05-24 13:05:10 +0200

    Shelf.hpp comments

--- src/Shelf.cpp
+++ src/Shelf.cpp
@@ -5,6 +5,16 @@
 Shelf::Shelf() {}
 
 bool Shelf::swapPallet(int slot, int slot2) {
+
+	// Moet binnen de array blijven.
+	if (slot > 4 || slot2 > 4){
+		return false;
+	}
+
+	// kan niet dezelfde positie veranderen van plek.
+	if (slot == slot2){
+		return false;
+	}
 	Pallet temp = pallets[slot];
 	pallets[slot] = pallets[slot2];
 	pallets[slot2] = temp;
--- src/include/Shelf.hpp
+++ src/include/Shelf.hpp
@@ -9,10 +9,33 @@ class Shelf : public IContainer{
 public:
 	std::array<Pallet, 4> pallets;
 
+	/**
+	 * @brief Construct a new Shelf object.
+	 */
 	Shelf();
 
+	/**
+	 * @brief Is the Shelf empty?
+	 * 
+	 * @return `true`: The Shelf is empty.
+	 * @return `false`: The Shelf is not empty
+	 */
 	bool isEmpty() override;
+
+	/**
+	 * @brief Is the Shelf full?
+	 * 
+	 * @return `true`: The Shelf is full.
+	 * @return `false`: The Shelf is not full.
+	 */
 	bool isFull() override;
+
+	/**
+	 * @brief Swap the position of two Pallet objects in the pallets array.
+	 * 
+	 * @return `true`: Positions are swapped successfully.
+	 * @return `false`: Positions are not swapped successfully.
+	 */
 	bool swapPallet(int slot, int slot2);
 
 };
